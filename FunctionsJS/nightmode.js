714b9067f39c988a93f9998502485f9cb413ff24
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-06-15 13:54:44 +0530

    added sound effect on mode change

--- FunctionsJS/nightmode.js
+++ FunctionsJS/nightmode.js
@@ -2,10 +2,13 @@ var body = document.getElementsByTagName("body")[0];
 var nightbtn = document.getElementById("night-mode");
 var boldTexts = document.querySelectorAll("[bold-text]");
 var sideBar = document.getElementById("colorlib-main-nav");
+var nightTune = new Audio("./music/s-night.mp3");
+var dayTune = new Audio("./music/s-day.mp3");
 let darkMode = localStorage.getItem("dark-mode");
 
 const disableDarkMode = () => {
   nightbtn.innerHTML = "Night Mode off";
+  dayTune.play();
   body.style.background = "#fafafa";
   [...boldTexts].forEach((boldtext) => {
     boldtext.style.color = "#f0f0f0";
@@ -16,6 +19,7 @@ const disableDarkMode = () => {
 
 const enableDarkMode = () => {
   nightbtn.innerHTML = `Night Mode on <i class="icon-moon"></i> `;
+  nightTune.play();
   body.style.background = "#1b1a1afa";
   [...boldTexts].forEach((boldtext) => {
     boldtext.style.color = "#8c8b8b";
@@ -26,6 +30,7 @@ const enableDarkMode = () => {
 
 if (darkMode === "enabled") {
   enableDarkMode(); // set state of darkMode on page load
+  nightTune.pause();
 }
 
 nightbtn.addEventListener("click", (e) => {
