5c763d1cb9ebbc86f4713b74fb78203c4d1b1f90
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-06-15 12:44:03 +0530

    fixed night mode not working on refresh

--- FunctionsJS/nightmode.js
+++ FunctionsJS/nightmode.js
@@ -2,25 +2,37 @@ var body = document.getElementsByTagName("body")[0];
 var nightbtn = document.getElementById("night-mode");
 var boldTexts = document.querySelectorAll("[bold-text]");
 var sideBar = document.getElementById("colorlib-main-nav");
-let isLight = true;
+let darkMode = localStorage.getItem("dark-mode");
 
-nightbtn.addEventListener("click", modeSwitch);
+const disableDarkMode = () => {
+  nightbtn.innerHTML = "Night Mode off";
+  body.style.background = "#fafafa";
+  [...boldTexts].forEach((boldtext) => {
+    boldtext.style.color = "#f0f0f0";
+  });
+  sideBar.style.background = "#fafafa";
+  localStorage.setItem("dark-mode", "disabled");
+};
 
-function modeSwitch() {
-  isLight = !isLight;
-  if (isLight) {
-    nightbtn.innerHTML = "Night Mode off";
-    body.style.background = "#fafafa";
-    [...boldTexts].forEach((boldtext) => {
-      boldtext.style.color = "#f0f0f0";
-    });
-    sideBar.style.background = "#fafafa";
-  } else {
-    nightbtn.innerHTML = `Night Mode on <i class="icon-moon"></i> `;
-    body.style.background = "#1b1a1afa";
-    [...boldTexts].forEach((boldtext) => {
-      boldtext.style.color = "#8c8b8b";
-    });
-    sideBar.style.background = "rgb(5 4 4 / 98%)";
-  }
+const enableDarkMode = () => {
+  nightbtn.innerHTML = `Night Mode on <i class="icon-moon"></i> `;
+  body.style.background = "#1b1a1afa";
+  [...boldTexts].forEach((boldtext) => {
+    boldtext.style.color = "#8c8b8b";
+  });
+  sideBar.style.background = "rgb(5 4 4 / 98%)";
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+if (darkMode === "enabled") {
+  enableDarkMode(); // set state of darkMode on page load
 }
+
+nightbtn.addEventListener("click", (e) => {
+  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
