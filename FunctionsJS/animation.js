644deb89f929c8228a6fded5e96a8fd8a51e30d8
basit mir <basitmir0222@gmail.com>
2023-01-26 14:27:34 +0530

    porfolio updated design

--- FunctionsJS/animation.js
+++ FunctionsJS/animation.js
@@ -1,40 +1,40 @@
-var helloText = document.getElementById("bold-hello");
-var musicBtn = document.getElementById("music-button");
-var audio = new Audio("./music/song.mp3");
-var isPlaying = false;
-var boldTexts = document.querySelectorAll("[bold-text]");
-var body = document.getElementsByTagName("body")[0];
+// var helloText = document.getElementById("bold-hello");
+// var musicBtn = document.getElementById("music-button");
+// var audio = new Audio("./music/song.mp3");
+// var isPlaying = false;
+// var boldTexts = document.querySelectorAll("[bold-text]");
+// var body = document.getElementsByTagName("body")[0];
 
-musicBtn.addEventListener("click", togglePlay);
-animateHello();
-function animateHello() {
-  helloText.classList.add("animate-hello");
-}
+// musicBtn.addEventListener("click", togglePlay);
+// animateHello();
+// function animateHello() {
+//   helloText.classList.add("animate-hello");
+// }
 
-function togglePlay() {
-  if (isPlaying) {
-    audio.pause();
-    [...boldTexts].forEach((boldtext) => {
-      boldtext.classList.remove("bold-text-animate");
-    });
-    body.classList.remove("body-dance");
-    musicBtn.innerHTML = `<i class="icon-music"></i>`;
-    musicBtn.style.color = "rgb(110, 110, 110)";
-  } else {
-    audio.play();
-    [...boldTexts].forEach((boldtext) => {
-      boldtext.classList.add("bold-text-animate");
-    });
+// function togglePlay() {
+//   if (isPlaying) {
+//     audio.pause();
+//     [...boldTexts].forEach((boldtext) => {
+//       boldtext.classList.remove("bold-text-animate");
+//     });
+//     body.classList.remove("body-dance");
+//     musicBtn.innerHTML = `<i class="icon-music"></i>`;
+//     musicBtn.style.color = "rgb(110, 110, 110)";
+//   } else {
+//     audio.play();
+//     [...boldTexts].forEach((boldtext) => {
+//       boldtext.classList.add("bold-text-animate");
+//     });
 
-    body.classList.add("body-dance");
+//     body.classList.add("body-dance");
 
-    musicBtn.style.color = "#fb5353";
-  }
-}
+//     musicBtn.style.color = "#fb5353";
+//   }
+// }
 
-audio.onplaying = function () {
-  isPlaying = true;
-};
-audio.onpause = function () {
-  isPlaying = false;
-};
+// audio.onplaying = function () {
+//   isPlaying = true;
+// };
+// audio.onpause = function () {
+//   isPlaying = false;
+// };
--- FunctionsJS/nightmode.js
+++ FunctionsJS/nightmode.js
@@ -1,46 +1,72 @@
-var body = document.getElementsByTagName("body")[0];
-var nightbtn = document.getElementById("night-mode");
-var boldTexts = document.querySelectorAll("[bold-text]");
-var sideBar = document.getElementById("colorlib-main-nav");
-var menuLine = document.getElementById("menu-line");
-var nightTune = new Audio("./music/s-night.mp3");
-var dayTune = new Audio("./music/s-day.mp3");
-let darkMode = localStorage.getItem("dark-mode");
+// var body = document.getElementsByTagName("body")[0];
+// var nightbtn = document.getElementById("night-mode");
+// var boldTexts = document.querySelectorAll("[bold-text]");
+// var sideBar = document.getElementById("colorlib-main-nav");
+// var menuLine = document.getElementById("menu-line");
+// var nightTune = new Audio("./music/s-night.mp3");
+// var dayTune = new Audio("./music/s-day.mp3");
+// let darkMode = localStorage.getItem("dark-mode");
 
-const disableDarkMode = () => {
-  nightbtn.innerHTML = "Night Mode off";
+// const disableDarkMode = () => {
+//   nightbtn.innerHTML = "Night Mode off";
 
-  dayTune.play();
-  body.classList.remove("nightBody");
-  [...boldTexts].forEach((boldtext) => {
-    boldtext.style.color = "#e4e4e4";
-  });
-  sideBar.style.background = "#fafafa";
-  localStorage.setItem("dark-mode", "disabled");
-};
+//   dayTune.play();
+//   body.classList.remove("nightBody");
+//   [...boldTexts].forEach((boldtext) => {
+//     boldtext.style.color = "#e4e4e4";
+//   });
+//   sideBar.style.background = "#fafafa";
+//   localStorage.setItem("dark-mode", "disabled");
+// };
 
-const enableDarkMode = () => {
-  nightbtn.innerHTML = `Night Mode on <i class="icon-moon"></i> `;
+// const enableDarkMode = () => {
+//   nightbtn.innerHTML = `Night Mode on <i class="icon-moon"></i> `;
 
-  nightTune.play();
-  body.classList.add("nightBody");
-  [...boldTexts].forEach((boldtext) => {
-    boldtext.style.color = "#8c8b8b ";
-  });
-  sideBar.style.background = "rgb(5 4 4 / 98%)";
-  localStorage.setItem("dark-mode", "enabled");
-};
+//   nightTune.play();
+//   body.classList.add("nightBody");
+//   [...boldTexts].forEach((boldtext) => {
+//     boldtext.style.color = "#8c8b8b ";
+//   });
+//   sideBar.style.background = "rgb(5 4 4 / 98%)";
+//   localStorage.setItem("dark-mode", "enabled");
+// };
 
-if (darkMode === "enabled") {
-  enableDarkMode(); // set state of darkMode on page load
-  nightTune.pause();
-}
+// if (darkMode === "enabled") {
+//   enableDarkMode(); // set state of darkMode on page load
+//   nightTune.pause();
+// }
 
-nightbtn.addEventListener("click", (e) => {
-  darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
-  if (darkMode === "disabled") {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
+// nightbtn.addEventListener("click", (e) => {
+//   darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+//   if (darkMode === "disabled") {
+//     enableDarkMode();
+//   } else {
+//     disableDarkMode();
+//   }
+// });
+
+var COLOR_one = document.getElementById("first-bg");
+var COLOR_two = document.getElementById("second-bg");
+var COLOR_three = document.getElementById("third-bg");
+var Body = document.getElementById("body");
+
+COLOR_one.addEventListener("mouseover", () => {
+  Body.style.background = "rgb(189,190,238)";
+});
+COLOR_one.addEventListener("mouseout", () => {
+  Body.style.background = "#e8e5e2";
+});
+
+COLOR_two.addEventListener("mouseover", () => {
+  Body.style.background = "rgb(245,151,148)";
+});
+COLOR_two.addEventListener("mouseout", () => {
+  Body.style.background = "#e8e5e2";
+});
+
+COLOR_three.addEventListener("mouseover", () => {
+  Body.style.background = "rgb(208,148,234)";
+});
+COLOR_three.addEventListener("mouseout", () => {
+  Body.style.background = "#e8e5e2";
 });
