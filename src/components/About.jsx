f93bfdda690914be8d3d5864da40cbb41a9d6ce4
basitmir7 <basitmir0222@gmail.com>
2026-07-31 01:28:36 +0530

    Animation changes and updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,7 @@ export default function About() {
             <h2 className="about__heading">
               <strong>{about.heading}</strong>
             </h2>
-            <p className="about__body">{about.body}</p>
+            <p className="about__body mono">{about.body}</p>
             <div className="about__cta">
               {about.cta.map((c, i) => (
                 <a
